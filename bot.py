64bb3e332310a333fff55b0da70579894c70a468
Denis Zharikov <ms-ms@yandex.ru>
2018-02-17 18:07:37 +0300

    bot

---
+++ bot.py
@@ -0,0 +1,28 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def greet_user(bot, update):
+    print('Вызван /start')
+    print(update)
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+def main():
+    updater = Updater("540429828:AAEj5WiGOKNyn0i_UcdwbVpdJZ-RSktC-ng")
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    updater.start_polling()
+    updater.idle()
+    
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)
+
+main()
